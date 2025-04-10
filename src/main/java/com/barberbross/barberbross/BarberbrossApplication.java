62fe00e4987b312498db21cf4ee746792441235a
Henrique Ferreira Polizer <polizerrickk@gmail.com>
2025-04-10 15:51:02 -0300

    Initial commit

---
+++ src/main/java/com/barberbross/barberbross/BarberbrossApplication.java
@@ -0,0 +1,13 @@
+package com.barberbross.barberbross;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberbrossApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarberbrossApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/barberbross/barberbross/BarberbrossApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barberbross.barberbross;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberbrossApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
